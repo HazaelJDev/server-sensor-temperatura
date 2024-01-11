7549455955bc5396a7c35f012501ac4022095cd2
HazaelJDev <jejibarrera@gmail.com>
2024-01-11 16:24:52 -0600

    Conexion con Johnny Five

--- index.js
+++ index.js
@@ -2,20 +2,18 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 3000;
-
 app.use(cors());
 
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const five = require('johnny-five');
+var SerialPort = require("serialport").SerialPort;
 
-let secondAdvance = 0;
+
 
 getTime = () => {
     let time = new Date();
-    let tiempo = time.getDate()+'/'+(time.getMonth()+1)+'/'+time.getFullYear()+'-'+time.getHours()+':'+time.getMinutes()+':'+(time.getSeconds()+secondAdvance);
-    secondAdvance = secondAdvance + 1;
-    if (secondAdvance >= 11) {
-        secondAdvance = 0;
-    }
-    
+    let tiempo = time.getDate()+'/'+(time.getMonth()+1)+'/'+time.getFullYear()+'-'+time.getHours()+':'+time.getMinutes()+':'+time.getSeconds();
     return tiempo;
 }
 
@@ -52,14 +50,55 @@ getData = () => {
 }
 
 let dummyData = [];
+let sensorData;
+
+// Configuración del puerto serial (ajusta el nombre del puerto según tu configuración)
+//const arduinoPort = new five.Port({ port: "COM3" });
+/*const arduinoPort = new SerialPort("COM3", {
+  baudrate: 9600,
+  buffersize: 1
+});*/
+
+// Inicializar el objeto de la placa
+//const board = new five.Board({ port: arduinoPort });
+const board = new five.Board({});
+
+
+// Esperar a que la placa esté lista
+board.on('ready', () => {
+  console.log('Arduino está listo.');
+  //serialPort = five.SerialPort(arduinoPort);
+  serialPort = five.SerialPort({
+    port: "COM3",
+    baudrate: 9600,
+  });
+
+  serialPort.on('open', () => {
+    console.log('Serial port opened!');
+  });
+
+  serialPort.on('data', (data) => {
+    console.log(`Data received: ${data}`);
+    sensorData = data;
+    // Enviar el valor del sensor a través de Socket.IO
+    io.emit('sensorData', data);
+  });
+  
+});
 
 // Ruta "/" de tipo GET que devuelve un JSON con datos ficticios
 app.get('/', (req, res) => {
-    dummyData = getData();
+    dummyData = [{TemperatureC: sensorData, Date: getTime()}];
   res.json(dummyData);
 });
 
+
+// Configuración de Socket.IO para la comunicación en tiempo real
+io.on('connection', (socket) => {
+  console.log('Cliente conectado');
+});
+
 // Iniciar el servidor en el puerto especificado
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Servidor Express escuchando en http://localhost:${port}`);
 });
